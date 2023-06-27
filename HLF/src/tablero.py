2ea2d94898d6fbd0ff79373418b67ee48d064772
Alberto Becerra <alberto.becerra@thebridgeschool.es>
2023-06-27 20:05:47 +0200

    Inicializacion y Board Size

--- HLF/src/tablero.py
+++ HLF/src/tablero.py
@@ -1,2 +1,16 @@
+import os, sys
+sys.path.append(os.getcwd())
+from HLF.utils import config
+
 class Tablero:
-    pass
+    BOARD_SIZE = config.BOARD_SIZE
+
+    def __init__(self, size=None):
+        if size is not None:
+            self.BOARD_SIZE = size
+
+
+if __name__ == "__main__":
+    
+    tablero_player = Tablero()
+    print(tablero_player)
